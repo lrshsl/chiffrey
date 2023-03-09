aad23a35b618383e10ee00c55937b984f6390da0
lrshsl <hoeslilars@gmail.com>
2023-03-09 22:51:07 +0100

    make pure html

--- scripts/caesar_vigenere.js
+++ scripts/caesar_vigenere.js
@@ -1,5 +1,24 @@
+document.querySelector(".translate-btn").addEventListener("click", exec_encode);
+const selected_mode = document.getElementById("cv-mode-select").value;
+document.getElementById("ceasar-input");
 
 
 function exec_encode() {
+    console.log("sent");
+    write_output("sie");
     return;
 }
+
+
+// Util/IO functions
+function write_output(out) {
+  const output_element = document.getElementsByClassName("output")[0];
+  const delimiter = document.getElementById("delimiter-input").value;
+  const prefix = document.getElementById("prefix-input").value;
+  const suffix = document.getElementById("suffix-input").value;
+
+  if (typeof(out) !== "string")
+    out = prefix + out.join(suffix + delimiter + prefix) + suffix;
+  output_element.textContent = out;
+}
+
--- scripts/html_include.js
+++ scripts/html_include.js
@@ -2,27 +2,19 @@
 html_include();
 
 function html_include() {
-    var element, file, xhttp;
 
-    // Execute `source(e)` for all elements with the `html-include` attribute
     include_elements = Array.from(document.querySelectorAll("*"))
-        .filter(function(e) {
-            e.getAttribute("html-include")})
-        .forEach(function(e) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.status == 200) {e.innerHTML = this.responseText;}
-                if (this.status == 200) {e.innerHTML = "Error 418: I'm a teapot"}
-            }
-            xhttp.open("GET", e.)
-        });
-
-    // for (var i = 0; i < include_elements.length; i++) {
-    //     element = include_elements[i];
-    //     console.log(element);
-
-    //     // xhttp.send();
-    //     return;
-    // }
+        .filter(e => e.getAttribute("html-include"))
+    include_elements.forEach(e => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            // console.log("Found " + e);
+            // console.log("Open " + e.getAttribute("html-include"));
+            if (this.status == 200) {e.innerHTML = this.responseText;}
+            if (this.status == 404) {e.innerHTML = "Error 418: I'm a teapot"}
+        }
+        xhttp.open("GET", e.getAttribute("html-include"));
+        xhttp.send();
+        e.removeAttribute("html-include");
+    });
 }
-
