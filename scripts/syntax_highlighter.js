c23f86a07e080c75886837ed30468f9f57c29dd0
lrshsl <hoeslilars@gmail.com>
2023-05-06 09:55:27 +0200

    syn-highlight for both keywords and special characters

--- scripts/syntax_highlighter.js
+++ scripts/syntax_highlighter.js
@@ -4,8 +4,9 @@ const gsyn_keywords_input = document.getElementById("syntax-highlighter_keywords
 const gsyn_literals_input = document.getElementById("syntax-highlighter_literals-input");
 const gsyn_tab_width_input = document.getElementById("syntax-highlighter_tab-width-input");
 
-const STR_REPLACE = `<span class="highlight_str">$1</span>`;
-const NUM_REPLACE = `<span class="highlight_num">$1</span>`;
+const STR_REPLACE = '<span class="highlight_str">$1</span>';
+const NUM_REPLACE = '<span class="highlight_num">$1</span>';
+
 
 function tab_width() {
     return gsyn_tab_width_input.value;
@@ -13,27 +14,25 @@ function tab_width() {
 
 function keywords() {
     return gsyn_keywords_input.value
-        .split(WHITESPACE_REGEX);
-        // .filter(word => word.length > 0);
+        .split(WHITESPACE_REGEX)
+        .filter(word => word.length > 0);
 }
 
 
 function refresh() {
     const kw_regex = new RegExp(`\\b(${keywords().join("|")})\\b`, "g");
     const str_regex = /("[^"]*")/g;
-    const num_regex = /([0-9]+)/g;
+    const num_regex = /([0-9]+\.?[0-9]*)/g;
     gsyn_bg_div.innerHTML = gsyn_editor.value
         // Fix newlines, tabs, and spaces
-        .replace(/[\n\r]/g, "<br>")
-        .replace(/\t/g, " ")
-        .replace(/ /g, "&nbsp;")
+        // .replace(/[\n\r]/g, "\n")
+        // .replace(/\t/g, " ")
+        // .replace(/ /g, " ")
         // Highlight literals
         .replace(str_regex, STR_REPLACE)
-        .replace(num_regex, NUM_REPLACE);
+        .replace(num_regex, NUM_REPLACE)
         // Highlight keywords
-        // .replace(kw_regex, `<span class="highlight_kw">$1</span>`);
-    // console.log(gsyn_editor.value);
-    // console.log(gsyn_bg_div.innerHTML);
+        .replace(kw_regex, `<span class="highlight_kw">$1</span>`);
 }
 
 // Prevent tab key from changing the focus
